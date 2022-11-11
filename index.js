f3f1d83664b0148ed7515a06390ecb6aa5672647
druidafox <vitorhugosjrp@gmail.com>
2022-11-11 09:50:29 -0300

    final changes

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var mysql = require('mysql')
 const jwt = require('jsonwebtoken');
 const SECRET = "DruidaFox";
 const sequelize = require('./db');
-const Bankslip = require('./client');
-var isEmpty = require('lodash.isempty');
+var config = require('./config');
+var routes = require('./routes')
 
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -20,7 +20,10 @@ var myConnection = require('express-myconnection')
  * Store database credentials in a separate config.js file
  * Load the file/module and its values
  */
-var config = require('./config')
+
+
+app.use('/',routes)
+
 var dbOptions = {
   host: config.database.host,
   user: config.database.user,
@@ -47,56 +50,7 @@ app.use(myConnection(mysql, dbOptions, 'single'));
   }
 })();
 
-app.get('/', (req, res) => {
-  req.getConnection(function (err, connection) {
-    if (err) return next(err);
-    var id = req.body.id;
-    var due_date = req.body.date;
-  })
-})
 
-app.get('/rest/bankslips/:id', (req, res) => {
-  if (req.body.user === 'vitorhugo' && req.body.password === '123') {
-    //auth ok
-    const id = req.params.id; //esse id viria do banco de dados
-    const token = jwt.sign({ id }, SECRET, {
-      expiresIn: 300 // expires in 5min
-    });
-    return res.json({ auth: true, token: token });
-  }
-
-  res.status(500).json({ message: 'Login inválido!' });
-})
-
-app.post('/rest/bankslips', async (req, res) => {  //Esse método deve receber um novo boleto e inseri-lo em um 
-  var bankslip = req.body;
-  if(isEmpty(req.body)) {
-    console.log('Bankslip not provided in the request body');
-    return res.status(400).json('Bankslip not provided in the request body')
-}
-
-  try {
-    const createBankslip = await Bankslip.create({       ///banco de dados para ser consumido pela própria API. Todos os campos são obrigatórios.
-      due_date: bankslip.due_date,
-      total_in_cents: bankslip.total_in_cents,
-      customer: bankslip.customer,
-      status: "PENDING"
-    });
-
-    res.status(200).json({
-      'id': createBankslip.id,
-      'due_date': createBankslip.due_date,
-      'total_in_cents': createBankslip.total_in_cents,
-      'customer': createBankslip.customer,
-      'status': createBankslip.status,
-    })
-
-  } catch (error) {
-    if (error) {
-      res.status(422).json('Invalid bankslip provided.The possible reasons are: A field of the provided bankslip was null or with invalid values')
-    }
-  }
-})
 
 
 
---
+++ routes.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var router = express.Router();
+var isEmpty = require('lodash.isempty');
+const sequelize = require('./db');
+var config = require('./config');
+var Bankslip = require('./client')
+
+
+    router.get('/', (req, res) => {
+        req.getConnection(function (err, connection) {
+          if (err) return next(err);
+          var id = req.body.id;
+          var due_date = req.body.date;
+        })
+      });
+      
+      router.get('/rest/bankslips/:id', (req, res) => {
+        if (req.body.user === 'vitorhugo' && req.body.password === '123') {
+          //auth ok
+          const id = req.params.id; //esse id viria do banco de dados
+          const token = jwt.sign({ id }, SECRET, {
+            expiresIn: 300 // expires in 5min
+          });
+          return res.json({ auth: true, token: token });
+        }
+      
+        res.status(500).json({ message: 'Login inválido!' });
+      });
+      
+      router.post('/rest/bankslips', async (req, res) => {  //Esse método deve receber um novo boleto e inseri-lo em um 
+        var bankslip = req.body;
+        if(isEmpty(req.body)) {                                                       //
+          console.log('Bankslip not provided in the request body');                   //
+          return res.status(400).json('Bankslip not provided in the request body')    //
+      }
+      
+        try {
+          const createBankslip = await Bankslip.create({       ///banco de dados para ser consumido pela própria API. Todos os campos são obrigatórios.
+            due_date: bankslip.due_date,
+            total_in_cents: bankslip.total_in_cents,
+            customer: bankslip.customer,
+            status: "PENDING"
+          });
+      
+          res.status(200).json({
+            'id': createBankslip.id,
+            'due_date': createBankslip.due_date,
+            'total_in_cents': createBankslip.total_in_cents,
+            'customer': createBankslip.customer,
+            'status': createBankslip.status,
+          });
+      
+        } catch (error) {
+          if (error) {
+            res.status(422).json('Invalid bankslip provided.The possible reasons are: A field of the provided bankslip was null or with invalid values')
+          }
+        }
+      });
+
+
+      module.exports = router;
