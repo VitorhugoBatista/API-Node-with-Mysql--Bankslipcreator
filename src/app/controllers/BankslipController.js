2a37d8ca9418fb68ae3192ee9530025981b68b2e
druidafox <vitorhugosjrp@gmail.com>
2023-01-22 09:40:09 -0300

    last

--- src/app/controllers/BankslipController.js
+++ src/app/controllers/BankslipController.js
@@ -1,17 +1,18 @@
 
 import Bankslipcreate from '../services/Bankslipcreate.js'
 import Bankslipdelete from '../services/Bankslipdelete.js'
+import Bankslipshow from '../services/Bankslipshow.js'
 
 class BankslipController {
   async show(req, res) {
-
+    const response = await Bankslipshow(req.params);
+    return res.json(response);
 
   }
 
   async create(req, res) {
     const response = await Bankslipcreate(req.body);
     return res.status(201).json(response);
-
   }
 
 
--- src/app/services/Bankslipshow.js
+++ src/app/services/Bankslipshow.js
@@ -1,59 +1,48 @@
+import { response } from "express";
 import moment from "moment/moment.js";
 import Bankslip from "../models/Bankslip.js";
+import Payment from "../models/Payment.js"
+import msg from "./UserConstants.js"
 
-console.log(moment().format())
+async function checkFine(due_date, payment_date, total_in_cents) {
+  let fine = []
+  let formulefine = ((payment_date - due_date) / (1000 * 60 * 60 * 24)) // calcula a diferença de dia entre a data de vencimento do boleto e a data de pagamento//
 
-async function show(req,res){
+  if (formulefine >= 10) {
+    fine = + (total_in_cents * 0.005)  ///
+  }
+  else if (formulefine <= 10)
+  
+  { fine = + (total_in_cents * 0.001) }
+  console.log(fine)
 
 
-let bankslip = await Bankslip.findByPk(req.params.id, { include: [{ as: 'payments', model: Payment }] });
+
+}
+
+
+
+async function show(req) {
+
+
+  try {
+    const bankslip = await Bankslip.findByPk(req.id, { include: [{ as: 'payments', model: Payment }] });
     
-    if (bankslip.status === "PENDING") {
-       res.json({
-        id: bankslip.id,
-        due_date: bankslip.due_date,
-        total_in_cents: bankslip.total_in_cents,
-        customer: bankslip.customer,
-        status: bankslip.status
-      })
-    }
+      let total_in_cents = bankslip.total_in_cents
+      let due_date = moment(bankslip.due_date)
+      let payment_date = moment(bankslip.payment_date)
+      checkFine(due_date, payment_date, total_in_cents)
+     response.status(200) 
+    return 
     
-      let due_date = new Date(bankslip.due_date)
-      let payment_date = new Date(bankslip.payments.payment_date);
-      let bankslip_payment_month = payment_date.getUTCMonth() + 1;
-      let bankslip_payment_day = payment_date.getUTCDate();
-      // let bankslip_payment_year = payment_date.getUTCFullYear();
-      let fine = []
-      let bankslip_due_date_month = due_date.getUTCMonth() + 1;
-      let bankslip_due_date_day = due_date.getUTCDate();
-      //let bankslip_due_date_year = due_date.getUTCFullYear();
-      console.log("line 29")
-      let hasFine = () => {
-        if (bankslip_payment_month <= bankslip_due_date_month || (bankslip_payment_day <= bankslip_due_date_day)) {
-          console.log("line 32")
-          return fine[0]
-        }
-        else if (bankslip_payment_month === bankslip_due_date_month || (bankslip_payment_day - bankslip_due_date_day > 10)) {
-          console.log("line 36")
-          fine.push((bankslip.total_in_cents * 0.005))
-          return fine[0]
-        }
-        else if (bankslip_payment_month === bankslip_due_date_month || (bankslip_payment_day - bankslip_due_date_day <= 10)) {
-          console.log("line 41")
-          fine.push(bankslip.total_in_cents * 0.01)
-          return fine[0]
-        }
-      }
-      res.json({
-        id: bankslip.id,
-        due_date: bankslip.due_date,
-        payment_date: bankslip.payments.payment_date,
-        total_in_cents: bankslip.total_in_cents,
-        customer: bankslip.customer,
-        fine: hasFine(),
-        status: bankslip.status,
-      })
+  } catch (error) {
+    response.status(error.status || 404);
+    return msg.bankslipNotFound
+  }
 
-    }
+}
 
-    export default show;
+
+
+
+export default show;
